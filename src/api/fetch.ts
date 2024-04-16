83c3e11f43bc1cc923ced9ab5eaca41a122295e8
Vitor Siqueira <vitor.siqueirr@gmail.com>
2024-04-16 15:04:07 -0300

    refactor

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,6 +1,7 @@
+const BASE_URL =
+  "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=10&sortBy=id&orderBy=ASC";
+
 export const fetchProducts = async () => {
-  const res = await fetch(
-    "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=10&sortBy=id&orderBy=ASC"
-  );
+  const res = await fetch(BASE_URL);
   return await res.json();
 };
